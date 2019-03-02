a4bfd0d3096945f2634ec035298e26ba83076f67
daniloheraclio <emaildedanilo@gmail.com>
2019-03-02 17:54:51 -0500

    removed comments about github

--- github.js
+++ github.js
@@ -25,20 +25,3 @@ class GitHub {
     }
   }
 }
-
-/* 
->>  OAuth2 key/secret
-    https://api.github.com/users/whatever?client_id=xxxx&client_secret=yyyy'
-
->>  GITHUB API USER - https://developer.github.com/v3/users/
-    https://api.github.com/users/:username?client_id=xxxx&client_secret=yyyy
-
->>  GITHUB API REPOS - https://developer.github.com/v3/repos/#list-user-repositories
-    https://api.github.com/users/:username/repos?client_id=xxxx&client_secret=yyyy
-
-    Parameters
-    type:       string: all, public, private, forks, sources, member: default is all
-    sort:       string: created, updated, pushed, full_name: default is created
-    direction:  string: asc, desc: default when using full_name is asc, otherwise desc
-
-*/
